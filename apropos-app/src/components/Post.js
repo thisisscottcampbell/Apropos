65e436baeac256dded6512f4cd39ed0555af2799
Scott Campbell <thisisscottcampbell@gmail.com>
2021-05-27 16:31:20 -0400

    update

--- apropos-app/src/components/Post.js
+++ apropos-app/src/components/Post.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import { UserContext } from '../App';
 
-const Post = ({ content, image, user }) => {
-	const currentUser = React.useContext(UserContext);
-	console.log(currentUser);
-	const isCurrentUser = currentUser === user;
-
+function Post({ image, content, user }) {
 	return (
 		<>
 			{image && (
 				<img
-					stlye={{ height: 100, width: 200, objectFit: 'cover' }}
+					style={{ height: 100, width: 200, objectFit: 'cover' }}
 					src={URL.createObjectURL(image)}
-					alt=""
+					alt="Post cover"
 				/>
 			)}
 			<p>{content}</p>
-			<div style={{ color: isCurrentUser && 'green' }}>{user}</div>
+			<div>{user}</div>
 		</>
 	);
-};
+}
 
 export default Post;
