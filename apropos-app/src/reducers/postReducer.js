c18c0f8769f9c2bc79143b3d26879210508ed930
Scott Campbell <thisisscottcampbell@gmail.com>
2021-05-27 17:17:15 -0400

    useReducer post

---
+++ apropos-app/src/reducers/postReducer.js
@@ -0,0 +1,14 @@
+const postReducer = (state, action) => {
+	switch (action.type) {
+		case 'ADD_POST': {
+			const newPost = action.payload.post;
+			return { posts: [newPost, ...state.posts] };
+		}
+		case 'DELETE_POST': {
+		}
+		default:
+			return state;
+	}
+};
+
+export default postReducer;
